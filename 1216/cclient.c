88a389f8d5a92fb16b103ca32ef50f1c2b036669
jiyun1615 <yunn1999@naver.com>
2020-12-17 19:48:12 -0800

    uncompleted update 1216

---
+++ 1216/cclient.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#define DEFAULT_PROTOCOL 0
+#define MAXLINE 100
+
+int main()
+{
+	int clientfd, result;
+	char inmsg[MAXLINE], outmsg[MAXLINE];
+	struct sockaddr_un serverAddr;
+	
+	clientfd = socket(AF_UNIX, SOCK_STREAM, DEFAULT_PROTOCOL);
+	serverAddr.sun_family = AF_UNIX;
+	strcpy(serverAddr.sun_path, "convert");
+
+	do {
+		result = connect(clientfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+		if(result == -1) sleep(1);
+	} while (result == -1);
+
+	printf("Type the string to be converted:\n");
+	fgets(inmsg, MAXLINE, stdin);
+	write(clientfd, inmsg, strlen(inmsg)+1);
+
+	readLine(clientfd, outmsg);
+	printf("%s --> \n%s", inmsg, outmsg);
+	close(clientfd);
+	exit(0);
+}
+
+readLine(int fd, char* str)
+{
+	int n;
+	do {
+		n = read(fd, str, 1);
+	} while(n>0 && *str++ != '\0');
+	return (n>0);
+}
+
